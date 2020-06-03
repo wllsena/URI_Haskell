852d94ef0dd1a560cfa8fb7d749e0a5e0f81e00f
wnsena <wnsena@protonmail.com>
2020-06-03 12:56:58 -0300

    1018 accepted

--- beginner/1018.hs
+++ beginner/1018.hs
@@ -1,9 +1,11 @@
+import           Text.Printf
+
 getOutput :: [Integer] -> String -> Integer -> String
-getOutput []       output _  = output
-getOutput (nt:nts) output rs =
-  getOutput nts output' (rs `rem` nt)
-  where output' = output ++ show (rs `div` nt) ++ " nota(s) de R$ " ++ show nt ++ ",00\n"
+getOutput []       text _  = text
+getOutput (nt:nts) text rs =
+  getOutput nts (text ++ line) (rs `rem` nt)
+  where line = printf "%d nota(s) de R$ %d,00\n" (rs `div` nt) nt
 
 main :: IO ()
 main = do reais <- readLn :: IO Integer
-          putStr $ getOutput [100, 50, 20, 10, 5, 2, 1] "" reais
+          putStr $ show reais ++ getOutput [100, 50, 20, 10, 5, 2, 1] "\n" reais
